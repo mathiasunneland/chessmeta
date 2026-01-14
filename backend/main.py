88be64bbb562ceac22036dacdd052eb8e14b078e
mathiasunneland <mathias.unneland@gmail.com>
2026-01-14 03:27:25 +0100

    Fix cross-origin error

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/stats/{username}")
 def get_chess_stats(username: str):
